857fafd7a6fe5a99b1bc72becc6847e686cbfdb7
Maxtrxn <pcmaxtron@gmail.com>
2024-12-03 11:29:25 +0100

    Add files via upload

--- CODE_ETUDIANT/PROJET/src/SERVICE/service.c
+++ CODE_ETUDIANT/PROJET/src/SERVICE/service.c
@@ -117,9 +117,7 @@ int main(int argc, char * argv[])
 
         // Notifier l'orchestre que le traitement est terminé
         struct sembuf sem_op = {0, 1, 0}; // Incrémenter le sémaphore
-        if (semop(cle_semaphore, &sem_op, 1) == -1) {
-            perror("Erreur lors de la notification à l'orchestre");
-        }
+        myassert(semop(cle_semaphore, &sem_op, 1) == -1, "Erreur lors de la notification à l'orchestre")
     }
 
     // libération éventuelle de ressources
